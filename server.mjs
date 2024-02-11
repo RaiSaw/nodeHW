9d9e44802ca624e1b1ddb86b37deac73c4a6b5a1
rai_S <raizadiannesawit@yahoo.com>
2024-02-11 16:53:56 +0100

    homework  extension

--- server.mjs
+++ server.mjs
@@ -1,39 +1,20 @@
-// HW > CRUD Express > Auth_Mongo-CRUD
+// API HW
 import express from 'express';
-import router from "./routes/model.mjs"
-import { models } from './utils/constants.mjs';
-import path from 'path';
-import { query } from "express-validator"
-import { MongoClient } from 'mongodb';
-import { connectToDB, getDB } from './mongo.mjs';
+import router from "./routes/homework.mjs"
 
-let db;
 
 const app = express()
 app.use(express.json())
-/* app.use(express.static('public')) */
-/* app.use(router); */
+app.use(router);
 
 const port = process.env.port || 3000;
 
-connectToDB((error) => {
-    if (!error) {
-        app.listen(port, () => {
-            console.log(`Server running on port ${port}`)
-        })
-        db = getDB();
-    }
-});
-
 app
-.get("/models", (req, res) => {
-    //toArray(), forEach()- iterate on db-documents
-    db.collection("models")
-        .find().toArray()
-        .then(resp => {
-            res.status(200).json(resp)
-        }).catch(err => {
-            res.status(500).json({error: "Oops, can't retrieve data!" })
-        })
+.get("/", (req, res) => {
+    res.status(201).send("Hey, wanna learn something cool 🌱?");
 })
-.post()
+
+.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
